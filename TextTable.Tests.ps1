5e071f227657fe746bd7ce383e356c2f6cdc56e5
Jon Carrier <jjcarrier@gmail.com>
2023-07-04 12:45:38 -0400

    Added tests

---
+++ TextTable.Tests.ps1
@@ -0,0 +1,41 @@
+# Basic tests for supporting various table formats
+
+BeforeAll {
+  Import-Module "$PSScriptRoot\TextTable.psm1"
+}
+
+Describe 'ConvertFrom-TextTable' {
+  It "returns an object only containing data from the first table when the pipelined text contains multiple tables with column-header separators" {
+    $table = Get-Content "$PSScriptRoot/TextTests/winget.txt" | ConvertFrom-TextTable
+    $table.Count | Should -Be 17
+  }
+
+  It "returns an object only containing data from the first table when the non-pipelined text contains multiple tables with column-header separators" {
+    $table = ConvertFrom-TextTable -Text (Get-Content "$PSScriptRoot/TextTests/winget.txt")
+    $table.Count | Should -Be 17
+  }
+
+  It "excludes and prevents further table parsing past the first -LastLineRegex match" {
+    $table = Get-Content "$PSScriptRoot/TextTests/winget.txt" | ConvertFrom-TextTable -LastLineRegEx "\d+ upgrades available."
+    $table.Count | Should -Be 16
+  }
+
+  It "treats the first row of -Text as the column header when -NoHeaderSeparator is set" {
+    $table = Get-Content "$PSScriptRoot/TextTests/no-column-separator.txt" | ConvertFrom-TextTable -NoHeaderSeparator
+    $table.Count | Should -Be 4
+  }
+
+  It "splits words that span across column boundaries" {
+    $table = Get-Content "$PSScriptRoot/TextTests/not-a-wellformed-table.txt" | ConvertFrom-TextTable -NoHeaderSeparator
+    $table.Count | Should -Be 1
+    $table.ColA | Should -Be "Anoth"
+    $table.ColB | Should -Be "er te"
+    $table.ColC | Should -Be "st case."
+  }
+
+  It "trims indentation from the first column's header and values" {
+    $table = Get-Content "$PSScriptRoot/TextTests/indented-numbers.txt" | ConvertFrom-TextTable
+    $table.Count | Should -Be 4
+    $table[0].Name | Should -Be "One"
+  }
+}
